51aa7c8135dcf06605cd9ff6835acad9959bd2d8
schavis2022 <schavis2022>
2022-11-21 13:38:57 -0500

    Variable ExerciseC32 DONE

---
+++ Variable-ExerciseC32/Program.cs
@@ -0,0 +1,19 @@
+﻿// Declaring and Initalize Variables
+string name = "Prince";
+int age = 12;
+char middleInitial = 'V';
+bool isOver18 = true;
+double currentWeight = 85;
+decimal currentBodyFat = 0.01m;
+
+//Interpolate these in a Console.WriteLine
+Console.WriteLine($"My dog's name is: {name}");
+Console.WriteLine($"My dog's age: {age}");
+Console.WriteLine($"His middle intial is: {middleInitial}");
+Console.WriteLine($"He is over 18: {isOver18}");
+Console.WriteLine($"CurrentWeight: {currentWeight}");
+Console.WriteLine($"CurrentBodyFat: {currentBodyFat}");
+
+
+Console.WriteLine("Hello, World!");
+
